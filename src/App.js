4640f6593c01ac40eab479726952ecd53f471e22
Morgan Costigan <morgan.costigan@gmail.com>
2018-12-26 17:38:03 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import fetch from 'node-fetch';
+
 
 class App extends Component {
+
+  // componentDidMount() {
+  //   fetch('http://ip-api.com/json/')
+  //     .then(res => res.json(),
+  //           console.log('json:', res))
+  //     .then(json => this.setState({ data: json }));
+  // };
+
+  constructor() {
+    super();
+
+    this.state = {
+      data: []
+    }
+
+    this.setStateHandler = this.setStateHandler.bind(this);
+  };
+
+  setStateHandler() {
+
+    fetch('http://ip-api.com/json/')
+      .then(res => res.json())      
+      .then(json => this.setState({ data: json.zip }))
+    console.log('state', this.state)
+
+  };
+  
+
   render() {
     return (
       <div className="App">
@@ -11,6 +41,10 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <div>
+            <button onClick={this.setStateHandler}>Click</button>
+            <h2>ZIP: {this.state.data}</h2>
+          </div>
           <a
             className="App-link"
             href="https://reactjs.org"
